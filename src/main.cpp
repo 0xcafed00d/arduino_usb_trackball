a44bd44a185b186014421a8b9029ccc269229ab1
Lee Witek <neomech@gmail.com>
2018-04-06 02:36:36 +0100

    factor out common code to updateAxis function and add consts to setup params

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ static const int PIN_X2 = 8;
 static const int PIN_Y1 = 3;
 static const int PIN_Y2 = 9;
 
+static const int32_t xScale = 10000;
+static const int32_t yScale = 10000;
+
+static const int xInverted = 0;
+static const int yInverted = 0;
+
 void xAxisISR();
 void yAxisISR();
 
@@ -13,11 +19,7 @@ volatile int32_t xcount = 0;
 volatile int32_t ycount = 0;
 
 void setup() {
-	while (!Serial) {
-	}
 	Mouse.begin();
-
-	Serial.begin(115200);
 
 	attachInterrupt(digitalPinToInterrupt(PIN_X1), xAxisISR, RISING);
 	attachInterrupt(digitalPinToInterrupt(PIN_Y1), yAxisISR, RISING);
@@ -36,36 +38,30 @@ void loop() {
 	interrupts();
 
 	if (x != 0 || y != 0)
-		Mouse.move(x / 10000, y / 10000);
+		Mouse.move(x / xScale, y / yScale);
+}
+
+void updateAxis(uint32_t& last, volatile int32_t& count, int pin, int inverted) {
+	uint32_t now = micros();
+	uint32_t val = now - last;
+	last = now;
+
+	if (val < 100000) {
+		val = 100000 - val;
+
+		if (digitalRead(pin) == inverted)
+			count += val;
+		else
+			count -= val;
+	}
 }
 
 void xAxisISR() {
-	static uint32_t lastMS = 0;
-	uint32_t now = micros();
-	uint32_t val = now - lastMS;
-	lastMS = now;
-
-	if (val < 100000) {
-		val = 100000 - val;
-		if (digitalRead(PIN_X2))
-			xcount -= val;
-		else
-			xcount += val;
-	}
+	static uint32_t last = 0;
+	updateAxis(last, xcount, PIN_X2, xInverted);
 }
 
 void yAxisISR() {
-	static uint32_t lastMS = 0;
-	uint32_t now = micros();
-	uint32_t val = now - lastMS;
-	lastMS = now;
-
-	if (val < 100000) {
-		val = 100000 - val;
-
-		if (digitalRead(PIN_Y2))
-			ycount -= val;
-		else
-			ycount += val;
-	}
+	static uint32_t last = 0;
+	updateAxis(last, ycount, PIN_Y2, yInverted);
 }
